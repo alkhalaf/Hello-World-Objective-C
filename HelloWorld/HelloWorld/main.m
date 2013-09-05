e47256351e02cb2fcd52e943ab48da15d395cf5e
Abdalla <abdalla@Abdallas-iMac.local>
2013-09-05 23:55:46 +0100

    initial Comment

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Abdalla on 05/09/2013.
+//  Copyright (c) 2013 Abdalla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // This is how to print a String on the screen.
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
